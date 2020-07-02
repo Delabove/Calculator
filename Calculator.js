6f643a3b172db55c3eeaa62e3c47c78824a500e5
DeLayne LaBove <delayne.labove@gmail.com>
2020-07-02 08:52:16 -0500

    Working on calculate function

--- Calculator.js
+++ Calculator.js
@@ -1,29 +1,58 @@
-// define constant and global
-//      Memory  = "0";      // initialize memory variable
-//     Current = "0";      //   and value of Display ("current" value)
-//     Operation = 0;      // Records code for eg * / etc.
-//     MAXLENGTH = 30;     // maximum number of digits before decimal!variables
-
-let memory = "0";
-let current = "0";
-let operation = 0;
-let maxLength = 30;
-
-
-// Add number to display
-function addNumber(num) {
-
-    if (current.length > maxLength) {
-        return "Too Long!"
-
-    } else if (current == 0 && current.indexOf(".") == -1) {
-        current = num;
-
-    } else {
-        return current + num;
-    }
-
-    document.getElementById("left-display").value = current;
-
-    ;
-}
+//
+//
+//
+// let current = "0";
+// let maxLength = 30;
+// let num1 = parseFloat(document.getElementById("left-display").value);
+// let op = document.getElementById("left-display").value;
+// let num2 = parseFloat(document.getElementById("left-display").value);
+// let screen = document.getElementById("left-display")
+//
+//
+// // Add number to display
+// function displayNumber(num) {
+//
+//     if (current.length > maxLength) {
+//         return "Too Long!"
+//
+//     } else if (current == 0 && current.indexOf(".") == -1) {
+//         current = num;
+//
+//     } else if (num2 == 0) {
+//             screen.value = "division by zero!"
+//         }
+//         switch (op) {
+//
+//             case '+':
+//                 let result = num1 + num2;
+//                 screen.value = result;
+//                 break;
+//
+//             case '-':
+//                 result = num1 - num2;
+//                 screen.value = result;
+//                 break;
+//
+//             case '*':
+//                 result = num1 * num2;
+//                 screen.value = result;
+//                 break;
+//
+//             case '/':
+//                 result = num1 / num2;
+//                 screen.value = result;
+//                 break;
+//
+//             case '%':
+//                 result = num1 % num2;
+//                 screen.value = result;
+//                 break;
+//
+//             default:
+//
+//                 break;
+//         }
+//
+//         document.getElementById("left-display").value = current;
+//
+// }
